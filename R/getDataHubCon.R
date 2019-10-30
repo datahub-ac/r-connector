1543e1ef5eac4f926c81ccf276a06321edcaa0e9
Kovacs Mate <matek@Kovacss-MacBook-Pro.local>
2019-10-30 11:37:13 +0100

    dbpath update

--- R/getDataHubCon.R
+++ R/getDataHubCon.R
@@ -58,3 +58,30 @@ getDataHubCon <- function(dbname, schemaname) {
   }
   return(con)
 }
+
+#' Function getDBPath()
+#' In AAP applications returns the database name and schema name
+#'
+#' @export
+getDBPath <- function() {
+  path_filename <- Sys.getenv("ACLIB_DBPATH_FILE", "/lifecycle/.dbpath")
+  if (!file.exists(path_filename))
+    stop(paste0('Could not find dbpath file ', path_filename, '.'))
+
+  con = file(path_filename, "r")
+  line = readLines(con, n = 1)
+  close(con)
+
+  if (length(line) == 0)
+    stop(paste0('Could not parse dbpath file, first line of ', path_filename, ' is empty.'))
+
+  split_arr <- unlist(strsplit(line, "\".\""))
+  if (length(split_arr) != 2 )
+    stop(paste0("Invalid path format in dbpath file ", path_filename, '.'))
+
+  db_name <- paste0(split_arr[1],"\"")
+  schema_name <- paste0("\"",split_arr[2])
+  sprintf("Found database = %s, schema = %s in dbpath file %s.", db_name, schema_name, path_filename)
+
+  return(list(dbname = db_name, schemaname = schema_name))
+}
--- README.md
+++ README.md
@@ -10,3 +10,10 @@ remotes::install_github("datahub-ac/r-connector")
 ```
 con <- datahub::getDataHubCon("DM", "SAMPLE_DB")
 ```
+
+# Usage in AAP applications
+
+```
+connection_info <- datahub::getDBPath()
+con <- datahub::getDataHubCon(connection_info$dbname, connection_info$schemaname)
+```
